73719623927e1f0f0151af8a1b0e798deae404f5
Silas Silva <silvasilas66@gmail.com>
2021-06-17 13:40:57 -0300

    Pulling routes on server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 const app = express();
 
 // Bodyparser middleware
@@ -14,6 +17,15 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("DataBase connected with success"))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes 
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port}!`));
