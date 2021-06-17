1d08238b333445327343b2c8a278de279146cb5c
Silas Silva <silvasilas66@gmail.com>
2021-06-17 19:20:36 -0300

    Components Navbar and Landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import './App.css';
 
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>Hello, World</h1>
+        <Navbar />
+        <Landing />
       </div> 
-    )
+    );
   }
 }
 
